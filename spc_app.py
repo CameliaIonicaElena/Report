0b44ce03664e0e50c94f609d6edaf9d516e14553
CameliaIonicaElena <camelia-elena.ionica@sig.biz>
2026-05-05 15:53:32 +0300

    Update spc_app.py

--- spc_app.py
+++ spc_app.py
@@ -18,9 +18,9 @@ st.title("SPC Dashboard")
 # LOAD DATA
 # =========================
 files = {
-    "Dataset 0": "Test-Measurements&Specs.xlsx",
-    "Dataset 1": "Test-Measurements&Specs1.xlsx",
-    "Dataset 2": "Test-Measurements&Specs2.xlsx"
+    "Dataset Original": "Test-Measurements&Specs.xlsx",
+    "Dataset Test1": "Test-Measurements&Specs1.xlsx",
+    "Dataset Test2": "Test-Measurements&Specs2.xlsx"
 }
 selected_dataset = st.sidebar.selectbox(
     "Select dataset",
